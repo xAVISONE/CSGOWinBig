88e885c937be0a89592924bbedd9fb249577a861
xAVISONE <xawisqo@gmail.com>
2016-02-22 16:03:46 +0100

    Update bot-withdraw.php

--- src/php/bot-withdraw.php
+++ src/php/bot-withdraw.php
@@ -3,7 +3,7 @@ include 'default.php';
 $db = getDB();
 
 # Get bot inventory
-$bot64Id = '76561198238743988';
+$bot64Id = '76561198036105876';
 $botInventory = json_decode(file_get_contents("https://steamcommunity.com/profiles/$bot64Id/inventory/json/730/2"), true);
 $rgInventory = $botInventory['rgInventory'];
 
